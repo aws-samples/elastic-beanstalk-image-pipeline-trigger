e7883b6a129b2c9256e9aba64cf6fa24b7eff1aa
Carlos Santos <csantos@users.noreply.github.com>
2021-06-16 12:49:51 -0700

    Updated file headers

--- src/BeanstalkImageBuilderPipeline/Repositories/IImageBuilderRepository.cs
+++ src/BeanstalkImageBuilderPipeline/Repositories/IImageBuilderRepository.cs
@@ -1,4 +1,9 @@
-﻿// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+﻿// This sample, non-production-ready project that provides provides the ability to detect when an Amazon Elastic Beanstalk 
+// platform's base AMI has been updated and starts an EC2 Image Builder Pipeline to automate the creation of a golden image.
+// © 2021 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.  
+// This AWS Content is provided subject to the terms of the AWS Customer Agreement available at  
+// http://aws.amazon.com/agreement or other written agreement between Customer and either
+// Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
 // SPDX-License-Identifier: MIT-0
 
 namespace BeanstalkImageBuilderPipeline.Repositories {
